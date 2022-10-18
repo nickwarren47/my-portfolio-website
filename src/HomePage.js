aae6998145c951d64e38daebd61c406de7652d87
Nick Warren <nickwarren47@gmail.com>
2022-10-18 15:53:03 -0600

    Added software languages to bottom of page

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,6 +7,15 @@ import divider from './image/divider.png'
 import email from './image/email.png'
 import github from './image/github.png'
 import linkedin from './image/linkedin.png'
+import javascript from './image/javascript.png'
+import react from './image/react.png'
+import ruby from './image/ruby.png'
+import rails from './image/rails.jpg'
+import postgres from './image/postgres.png'
+import sqlite from './image/sqlite.png'
+import html from './image/html.png'
+import css from './image/css.png'
+
 
 function HomePage(){
 
@@ -33,7 +42,7 @@ function HomePage(){
                     </div>
                 </div>
                 <div className="flex items-center justify-center">
-                    <div className="my-10 container flex justify-center">
+                    <div className="my-6 container flex justify-center">
                         <h3 className="text-center text-3xl font-bold font-mono w-8/12">About Me
                         <p className="text-2xl font-bold font-mono "> In 2016, I graduated from the University of Colorado with a Bachelors of Arts degree in Geology and a minor in Business Administration. While at CU, 
                         I wrote and defended my honors thesis infront of a panel of 4 professors. After successfuly defending my thesis, I graduated with Cum Laude honors. I then worked for the oil and gas industry for 5 years. At my Geological Technician position at 
@@ -52,9 +61,17 @@ function HomePage(){
                 </div>
                 <div className="flex items-center justify-center">
                     <div className="inline-flex">
-                        <p className="text-center text-3xl font-bold font-mono ml-2"><img className="inline h-9 w-9 mr-2" src={processor} alt=""/>Software Engineer /</p> 
-                        <p className="text-center text-3xl font-bold font-mono ml-2"><img className="inline h-9 w-9 mr-2" src={scientist} alt=""/>Scientist /</p>
-                        <p className="text-center text-3xl font-bold font-mono ml-2"><img className="inline h-9 w-9 mr-2" src={oxygen} alt=""/>Scuba Diver</p>
+                        <div className="">
+                            <p className="text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={javascript} alt=""/>JavaScript</p> 
+                        </div>
+                        {/* <p className="text-center text-2xl font-bold font-mono ml-10"><img className="h-20 w-20 mr-10 flex items-center justify-center" src={javascript} alt=""/>JavaScript</p>  */}
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={react} alt=""/>React</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={ruby} alt=""/>Ruby</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={rails} alt=""/>Rails</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={postgres} alt=""/>Postgresql</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={sqlite} alt=""/>SQLite3</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={html} alt=""/>HTML</p>
+                        <p className="text-center text-2xl font-bold font-mono ml-16"><img className="h-20 w-20" src={css} alt=""/>CSS</p>
                     </div>
                 </div>
             </div>
