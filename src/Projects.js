6efdadae4ba1b37814d94cee58615eaa2a73e720
Nick Warren <nickwarren47@gmail.com>
2022-10-26 14:28:38 -0600

    Added descriptions to projects and github links to Destiny

--- src/Projects.js
+++ src/Projects.js
@@ -59,6 +59,14 @@ function Projects(){
         window.open("https://coders-keys.netlify.app/");
     }
 
+    function DestinyGitHubBack() {
+        window.open("https://github.com/nickwarren47/phase-3-sinatra-react-project");
+    }
+
+    function DestinyGitHubFront() {
+        window.open("https://github.com/nickwarren47/sinatra-react-project-front-end");
+    }
+
     return(
         <div>
             <div className='bg-[url(https://user-images.githubusercontent.com/106715328/196836326-7bdc25c5-ff5a-451c-8710-2ead27475db7.jpg)] min-h-screen w-full bg-cover bg-center p-1'>
@@ -93,7 +101,9 @@ function Projects(){
                         <br/>
                         <h5 className="mb-2 text-3xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Wanderlust Travel App</h5>
                         <div className="p-4">
-                            <p className="font-medium text-lg text-black mb-3 dark:text-gray-400 pb-4" >This app allows the user to view various countries and to leave comments for each travel destination. The </p>
+                            <p className="font-medium text-lg text-black mb-3 dark:text-gray-400 pb-4" >This app allows the user to view various countries and to leave comments for each travel destination. The user can log in to their profile which then allows them to leave and edit their own reviews for each individual country. The user 
+                            can also choose an image to customize their profile. The user has the ability to search various countries based on name. Once they find their country of choice, they can select the country where they will see a photo of the country, previous reviews, and the ability to leave their own review. In the "Add a Review" section, they can then 
+                            add a review in the form and, using backened validations, the user must add a travel site, pro tip, length of stay, and a photo. The backend of the app uses BCRYPT encryption, validations, authorization, and authentication to protect user data.</p>
                         </div>
                         <h2 className="text-center text-xl font-bold"> Software Languages/Frameworks used: </h2>
                            <p className="text-center text-xl font-medium pb-4"> React | Ruby | Ruby on Rails | Tailwind CSS | JSON Routing | Internal API</p>
@@ -133,7 +143,9 @@ function Projects(){
                         <br/>
                         <h5 className="mb-2 text-3xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">Coder's Keys App</h5>
                         <div className="p-4">
-                            <p className="font-medium text-lg text-black mb-3 dark:text-gray-400 pb-4" >This app allows the user </p>
+                            <p className="font-medium text-lg text-black mb-3 dark:text-gray-400 pb-4" >This app allows the user to view and search for various shortcut keys for Mac OS, Windows, Linux, and VS Code. The shortcut keys are displayed by small cards that have the short cut keys needed to run command, the category, 
+                            the action the short cut key commences, and the source button to be linked to the site where the shortcut key is found. On each page (Windows, Mac OS, Linux, etc.), the user can select a category to filter by or can filter based on command action. The user can also add hotkeys in the "Form" tab. The app is a solely 
+                            frontend React app with a db.json internal API and JSON fetching to retrieve the data. The app uses Semantic UI for styling. </p>
                         </div>
                         <h2 className="text-center text-xl font-bold"> Software Languages/Frameworks used: </h2>
                            <p className="text-center text-xl font-medium pb-4"> React | Active Record | DB.json Database | Semantic UI | JSON Routing</p>
@@ -159,8 +171,8 @@ function Projects(){
                            <p className="text-center text-xl font-medium pb-4"> React | Ruby | Sinatra | Semantic UI | JSON Routing | Internal API</p>
                         <div className="flex items-center justify-center">
                             <div className="inline-flex my-5">
-                                <p className="text-center text-xl font-bold font-mono ml-12"><img className="inline h-7 w-7 mr-2" src={github} alt=""/>GitHub Link (Frontend)</p>
-                                <p className="text-center text-xl font-bold font-mono ml-12"><img className="inline h-7 w-7 mr-2" src={github} alt=""/>GitHub Link (Backend)</p>
+                                <p className="text-center text-xl font-bold font-mono ml-12"><img className="inline h-7 w-7 mr-2" src={github} alt=""/><Link className="text-black hover:text-blue-700 transition duration-300 ease-in-out mb-4" onClick={DestinyGitHubFront}>GitHub Link (Frontend)</Link></p>
+                                <p className="text-center text-xl font-bold font-mono ml-12"><img className="inline h-7 w-7 mr-2" src={github} alt=""/><Link className="text-black hover:text-blue-700 transition duration-300 ease-in-out mb-4" onClick={DestinyGitHubBack}>GitHub Link (Backend)</Link></p>
                                 <p className="text-center text-xl font-bold font-mono ml-12"><img className="inline h-7 w-7 mr-2" src={youtube} alt=""/>Demo Link</p>
                             </div>
                         </div>
